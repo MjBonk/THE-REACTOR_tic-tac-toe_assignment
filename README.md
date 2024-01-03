38f4fabef137c57166ff7c3b06ccc0aa7f1d1742
Maya Jerndahl <144351614+MjBonk@users.noreply.github.com>
2024-01-03 16:20:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# React + Vite
+
+Read through and implement the tutorial.
+
+Preferably, develop in a local environment, then push to GitHub and submit the link. (See: the second "Note" in the tutorial.) If you have difficulty developing the tutorial in local, you can also fork in CodeSandbox and submit a link to your sandbox, but please try to do it in local first and message our Slack channel if you are unable to do it.
+
+Bonus: Make an improvement. Either refactor some part of the code to increase readability/maintainability, or add a feature, or improve the styling / UI design. Tell us what you have improved in the comments, attach a screenshot if relevant.
+
+Also, if needed, make a comment here with:
+Anything that you haven't managed to get working. Include screenshots / error messages.
+Any ideas or pieces of code that don't make sense to you in this tutorial.
+Ideas or code elements that you'd like us to explain deeper in the upcoming sessions.
---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import "./App.css";
+import { useState } from "react";
+
+
+function Square({ value, onSquareClick }) {
+	return (
+		<button onClick={onSquareClick} className="square">
+			{value}
+		</button>
+	);
+}
+
+export default function Board() {
+  //useState hooks to remember turn and placement 
+	const [squares, setSquares] = useState(Array(9).fill(null));
+	const [xIsNext, setXIsNext] = useState(true);
+
+	function handleClick(i) {
+		console.log(squares);
+		if (squares[i] || calculateWinner(squares)) {
+			return;
+		}
+		const nextSquares = squares.slice();
+
+		if (xIsNext === true) {
+			nextSquares[i] = "X";
+		} else if (xIsNext === false) {
+			nextSquares[i] = "O";
+		}
+
+		setSquares(nextSquares);
+		setXIsNext(!xIsNext);
+	}
+
+	const winner = calculateWinner(squares);
+
+	return (
+		<div className="board-grid">
+			<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+			<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+			<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+			<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+			<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+			<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+			<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+			<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+			<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+		</div>
+	);
+}
+
+
+
+function calculateWinner(squares) {
+	//win combinations
+	const winLines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let i = 0; i < winLines.length; i++) {
+		//destructure the current line into three variebles
+		const [a, b, c] = winLines[i];
+
+		//if b and c has the same value as a (X/O)
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			//return winning symbol
+			return squares[a];
+		}
+	}
+
+	//else return null
+	return null;
+}
